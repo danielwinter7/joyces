3b04b37f6a6c6022677a3c9f4569dbb2fa131a2e
Daniel Winter <DanielW@Daniels-MacBook-Pro.local>
2016-01-15 18:26:14 +0100

    try to validate data

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,20 +1,35 @@
 Template.header.onRendered(function () {
-	var btnHelp = new Vue({
-  // options
-	});
+	var help = new Vue({
+	    el: '#help-wrapper',
+	    data: {
+	        help: false
+	    }
+	})
 });
 
 Template.index.onRendered(function () {
-	var surveyWrapper = new Vue({
-		el: '#survey-wrapper',
-		data: {
-			showQuestion: false
-		},
-		methods: {
-			showSurvey: function (event) {
-				showQuestion: true
-			}
-		}
-		});
 
+	var survey = new Vue({
+	    el: '#survey-wrapper',
+	    data: {
+	        survey: false 
+	    },
+	    methods: {
+	      handleIt: function (e) {
+	        if(!this.question.title)
+	          return alert('Title can\'t be blank!');
+
+	        // this.project.set('title', this.project.title);
+
+	        // if (this.project.validate()) {
+	        //   this.project.save();
+	        //   FlowRouter.go('projects_index');
+	        // }
+	        // else {
+	        //   return alert('Validation Error!')
+	        // }
+	      	e.preventDefault();
+	      }
+	    }
+	});
 });
