46691855a6bb1373087e85c37ea35c0cda2e5fb9
Daniel Winter <DanielW@Daniels-MacBook-Pro.local>
2016-01-15 14:10:23 +0100

    add logo

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,3 +1,20 @@
 Template.header.onRendered(function () {
+	var btnHelp = new Vue({
+  // options
+	});
+});
+
+Template.index.onRendered(function () {
+	var surveyWrapper = new Vue({
+		el: '#survey-wrapper',
+		data: {
+			showQuestion: false
+		},
+		methods: {
+			showSurvey: function (event) {
+				showQuestion: true
+			}
+		}
+		});
 
 });
